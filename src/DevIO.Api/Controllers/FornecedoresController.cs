b9ee928ad3e875a670bb404b3b51a00b8cb5dc0e
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-01-11 21:04:09 -0300

    Realizando CRUD de fornecedores

--- src/DevIO.Api/Controllers/FornecedoresController.cs
+++ src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -2,6 +2,7 @@
 using DevIO.Api.Controllers.Base;
 using DevIO.Api.ViewModels;
 using DevIO.Business.Interfaces.Services;
+using DevIO.Business.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevIO.Api.Controllers
@@ -13,16 +14,77 @@ namespace DevIO.Api.Controllers
         private readonly IMapper _mapper;
 
         public FornecedoresController(
-            IFornecedorService fornecedorService, 
+            IFornecedorService fornecedorService,
             IMapper mapper)
         {
             _fornecedorService = fornecedorService;
             _mapper = mapper;
         }
 
+        [HttpGet]
         public async Task<IEnumerable<FornecedorViewModel>> GetAllAsync()
         {
             return _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorService.GetAllAsync());
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> GetbyIdAsync(Guid id)
+        {
+            var fornecedorViewModel = await GetFornecedorProdutosEnderecoAsync(id);
+
+            if (fornecedorViewModel == null) return NotFound();
+
+            return fornecedorViewModel;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<FornecedorViewModel>> CreateAsync(FornecedorViewModel fornecedorViewModel)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var fornecedor = _mapper.Map<Fornecedor>(fornecedorViewModel);
+            var result = await _fornecedorService.AddAsync(fornecedor);
+
+            if (!result) return BadRequest();
+
+            return Ok(fornecedor);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> UpdateAsync(Guid id, FornecedorViewModel fornecedorViewModel)
+        {
+            if (id != fornecedorViewModel.Id || !ModelState.IsValid) return BadRequest();
+
+            var fornecedor = _mapper.Map<Fornecedor>(fornecedorViewModel);
+            var result = await _fornecedorService.UpdateAsync(fornecedor);
+
+            if (!result) return BadRequest();
+
+            return Ok(fornecedor);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<Fornecedor>> DeleteAsync(Guid id)
+        {
+            var fornecedor = await GetFornecedorEnderecoAsync(id);
+
+            if (fornecedor != null) return NotFound();
+
+            var result = await _fornecedorService.DeleteAsync(id);
+
+            if (!result) return BadRequest();
+
+            return Ok(fornecedor);
+        }
+
+        private async Task<FornecedorViewModel> GetFornecedorProdutosEnderecoAsync(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorService.GetFornecedorProdutosEnderecoAsync(id));
+        }
+
+        private async Task<FornecedorViewModel> GetFornecedorEnderecoAsync(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorService.GetFornecedorEnderecoAsync(id));
         }
     }
 }
--- src/DevIO.Business/Services/FornecedorService.cs
+++ src/DevIO.Business/Services/FornecedorService.cs
@@ -36,31 +36,35 @@ namespace DevIO.Business.Services
             return await _fornecedorRepository.GetFornecedorProdutosEnderecoAsync(id);
         }
 
-        public async Task AddAsync(Fornecedor fornecedor)
+        public async Task<bool> AddAsync(Fornecedor fornecedor)
         {
             if (!Validate(new FornecedorValidator(), fornecedor) ||
-                !Validate(new EnderecoValidator(), fornecedor.Endereco)) return;
+                !Validate(new EnderecoValidator(), fornecedor.Endereco)) return false;
 
             if (_fornecedorRepository.SearchAsync(f => f.Documento == fornecedor.Documento).Result.Any())
             {
                 Notify("Já existe um fornecedor com este documento informado.");
-                return;
+                return false;
             }
 
             await _fornecedorRepository.AddAsync(fornecedor);
+
+            return true;
         }
 
-        public async Task UpdateAsync(Fornecedor fornecedor)
+        public async Task<bool> UpdateAsync(Fornecedor fornecedor)
         {
-            if (!Validate(new FornecedorValidator(), fornecedor)) return;
+            if (!Validate(new FornecedorValidator(), fornecedor)) return false;
 
             if (_fornecedorRepository.SearchAsync(f => f.Documento == fornecedor.Documento && f.Id != fornecedor.Id).Result.Any())
             {
                 Notify("Já existe um fornecedor com este documento informado.");
-                return;
+                return false;
             }
 
             await _fornecedorRepository.UpdateAsync(fornecedor);
+
+            return true;
         }
 
         public async Task UpdateEnderecoAsync(Endereco endereco)
@@ -70,15 +74,21 @@ namespace DevIO.Business.Services
             await _enderecoRepository.UpdateAsync(endereco);
         }
 
-        public async Task DeleteAsync(Guid id)
+        public async Task<bool> DeleteAsync(Guid id)
         {
             if (_fornecedorRepository.GetFornecedorProdutosEnderecoAsync(id).Result.Produtos.Any())
             {
                 Notify("O fornecedor possui produtos cadastrados!");
-                return;
+                return false;
             }
 
+            var endereco = await _enderecoRepository.GetEnderecoByFornecedorAsync(id);
+
+            if (endereco != null) await _enderecoRepository.DeleteAsync(endereco.Id);
+
             await _fornecedorRepository.DeleteAsync(id);
+
+            return true;
         }
 
         public void Dispose()
