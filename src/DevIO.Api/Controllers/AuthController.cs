677cd1afeac859402005f72b09ac926f7a4a6754
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-01-16 20:52:13 -0300

    Controller de autenticação e dtos para o cadatro e login

---
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -0,0 +1,67 @@
+﻿using DevIO.Api.Controllers.Base;
+using DevIO.Api.Dtos;
+using DevIO.Business.Interfaces.Notifications;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api")]
+    public class AuthController : MainController
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(
+            SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager,
+            INotifier notifier) : base(notifier)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("new-account")]
+        public async Task<ActionResult> RegisterAsync(RegisterUserDto registerUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return CustomResponse(registerUser);
+            }
+
+            foreach (var error in result.Errors) ReportError(error.Description);
+
+            return CustomResponse(registerUser);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> LoginAsync(LoginUserDto loginUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
+
+            if (result.Succeeded) return CustomResponse(loginUser);
+
+            if (result.IsLockedOut)
+            {
+                ReportError("Usuário temporariamente bloqueado por tentativas inválidas");
+                return CustomResponse(loginUser);
+            }
+
+            ReportError("Usuário ou Senha incorretos");
+            return CustomResponse(loginUser);
+        }
+    }
+}
