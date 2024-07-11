958d7b0e7a697f404fda12c380e49c9923b415d1
leonardoqueirozrocha <leonardo.queiroz.rocha@outlook.com>
2024-07-11 12:36:21 -0400

    refactor: Alterando configuração do appsettings

--- src/DevIO.Business/Configurations/AppSettings.cs
+++ src/DevIO.Business/Configurations/AppSettings.cs
@@ -2,8 +2,5 @@
 
 public class AppSettings
 {
-    public string Secret { get; set; }
-    public int ExpirationHours { get; set; }
-    public string Issuer { get; set; }
-    public string ValidIn { get; set; }
+    public AuthConfiguration AuthConfiguration { get; set; }
 }
