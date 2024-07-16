fd4c4d140850b38de091e47571f92d829a52f380
leonardoqueirozrocha <leonardo.queiroz.rocha@outlook.com>
2024-07-16 00:18:33 -0400

    tests: init unit tests projects

---
+++ tests/DevIO.Business.Tests/Services/AuthServiceTests.cs
@@ -0,0 +1,17 @@
+namespace DevIO.Business.Tests.Services;
+
+public class AuthServiceTests
+{
+    private const string ClassName = nameof(AuthServiceTests);
+
+    [Fact(DisplayName = $"{ClassName} CreateAsync Should Generate Jwt Successfully")]
+    public async Task CreateAsync_ShouldGenerateJwtSuccessfully()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+}
